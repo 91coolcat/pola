7b8a0cda56e43ac0d75d2e34986a39356f7cad5a
home <feitianniao2016@outlook.com>
2021-02-12 11:39:58 +0800

    添加项目文件。

---
+++ 飞行棋/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 飞行棋
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
